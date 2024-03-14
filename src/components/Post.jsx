504fcf8a0d74a9e8beec18423065eb214ee8fee3
broom1337 <160509766+broom1337@users.noreply.github.com>
2024-03-14 17:03:27 +0300

    Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,51 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Card } from 'antd';
+import { Card, List } from 'antd';
+import './styles.css'; 
 
 export const Post = () => {
-  const { postId } = useParams();
-  const [post, setPost] = useState(null);
-  const [user, setUser] = useState(null);
-  const [comms, setComm] = useState(null);
+    const { postId } = useParams();
+    const [post, setPost] = useState(null);
+    const [user, setUser] = useState(null);
+    const [comments, setComments] = useState([]);
 
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-      .then(response => response.json())
-      .then(data => setPost(data));
-  }, [postId]);
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then(response => response.json())
+            .then(data => setPost(data));
+    }, [postId]);
 
-  useEffect(() => {
-    if(post) {
-      fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
-        .then(response => response.json())
-        .then(data => setUser(data));
-    }
-  }, [post]);
-  
-  useEffect(() => {
-      fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
-        .then(response => response.json())
-        .then(data => setComm(data));
-  }, [comms]);
+    useEffect(() => {
+        if (post) {
+            fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+                .then(response => response.json())
+                .then(data => setUser(data));
+        }
+    }, [post]);
 
-  return (
-    <div>
-      {post && user && comms &&(
-        <Card
-          title={user.name}
-          style={{
-            width: 300,
-            margin: 'auto',
-            border: '1px solid black',
-          }}
-        >
-          <p>{post.title}</p>
-          <p>{post.body}</p>
+    useEffect(() => {
+        if (post) {
+            fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+                .then(response => response.json())
+                .then(data => setComments(data));
+        }
+    }, [post]);
 
-          <p>{comms.name}</p>
-          <p>{comms.body}</p>
-        </Card>
-      )}
-    </div>
-  );
+    return (
+        <div className="card-container">
+            {post && user && (
+                <Card
+                    title={user.name}
+                    className="post-card"
+                >
+                    <p>{post.title}</p>
+                    <p>{post.body}</p>
+                </Card>
+            )}
+
+            <div className="card-container">
+                {comments.length > 0 && (
+                    <Card
+                        title={<div className="comments-header">Comments</div>}
+                        className="comments-card"
+                    >
+                        <List
+                            dataSource={comments}
+                            renderItem={item => (
+                                <List.Item>
+                                    <strong>{item.name}</strong>: {item.body}
+                                </List.Item>
+                            )}
+                        />
+                    </Card>
+                )}
+            </div>
+        </div>
+    );
 };
